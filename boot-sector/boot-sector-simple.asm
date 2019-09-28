e771c9697e2579d2f42f17e119177d3c65e98af0
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-09-28 16:09:12 +0100

    Add gitignore

--- boot-sector/boot-sector-simple.asm
+++ boot-sector/boot-sector-simple.asm
@@ -7,5 +7,6 @@ times 510-($-$$) db 0
 
 ; Magic constant for BIOS to check byte alignment, i.e. boot sector is in fact
 ; loaded in the first sector of the storage medium. Required that bytes 511
-; and 512 of the candidate boot sector are exactly 0xAA55.
+; and 512 of the candidate boot sector are exactly 0xAA55. Should this check
+; pass, then the BIOS can determine that the residing disk is bootable.
 dw 0xAA55
