e3b598e5a1ef0dd84ae93db5ba30c56ede6bb440
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-09-28 15:58:04 +0100

    Add simple boot sector

---
+++ boot-sector/boot-sector-simple.asm
@@ -0,0 +1,11 @@
+; Infinite loop (ED FD FF)
+loop:
+    jmp loop
+
+; Pad 510 zeros modulo infinite loop
+times 510-($-$$) db 0
+
+; Magic constant for BIOS to check byte alignment, i.e. boot sector is in fact
+; loaded in the first sector of the storage medium. Required that bytes 511
+; and 512 of the candidate boot sector are exactly 0xAA55.
+dw 0xAA55
